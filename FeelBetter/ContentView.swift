598b4c4f44d230e10fb9c1b44982ca8a9783baa1
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2020-01-28 22:13:05 -0500

    Adding XCode Project to the repo

---
+++ FeelBetter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FeelBetter
+//
+//  Created by tarek ait hamouda on 2020-01-28.
+//  Copyright © 2020 TAREKAMEHA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
